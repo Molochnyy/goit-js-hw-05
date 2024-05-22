08c1f9af9c83e39d7936572d71b833ee40df47a6
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2024-05-22 07:58:02 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        profile.username = newName;
+    },
+    updatePlayTime(hours) {
+        profile.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+};
+
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
